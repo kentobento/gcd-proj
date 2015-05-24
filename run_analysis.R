bd0d4457289e12f0abecc256f4090a2aaac6c8f3
Kent Koyanagi <kentkoyanagi@gmail.com>
2015-05-24 13:35:57 -0700

    added newlines to distinguish blocks of logic

--- run_analysis.R
+++ run_analysis.R
@@ -2,9 +2,11 @@
 activity.labels <- read.table("UCI HAR Dataset/activity_labels.txt")$V2
 feature.labels <- read.table("UCI HAR Dataset/features.txt")$V2
 
+
 # extract indices and names of features that end with mean() or std()
 mean.or.std.feature.indices <- grepl("mean()",feature.labels, fixed = TRUE) | grepl("std()", feature.labels, fixed = TRUE)
 mean.or.std.feature.labels <- feature.labels[mean.or.std.feature.indices]
+
 
 # read training and test set components
 train.X <- read.table("UCI HAR Dataset/train/X_train.txt")[, mean.or.std.feature.indices]
@@ -14,9 +16,11 @@ test.X <- read.table("UCI HAR Dataset/test/X_test.txt")[, mean.or.std.feature.in
 test.y <- activity.labels[unlist(read.table("UCI HAR Dataset/test/y_test.txt"))]
 test.subject <- read.table("UCI HAR Dataset/test/subject_test.txt")
 
+
 # construct training and test sets from respective components
 training.set <- cbind(train.subject, train.y, train.X)
 test.set <- cbind(test.subject, test.y, test.X)
+
 
 # merge training and test sets (after renaming second columns to avoid rbind error)
 activity.header.label <- "Activity"
@@ -40,8 +44,10 @@ corrected.mean.or.std.feature.labels <- gsub("std", "Std", corrected.mean.or.std
 # relabel combined data set with descriptive names
 names(combined.set) <- c("Subject", activity.header.label, corrected.mean.or.std.feature.labels)
 
+
 # compute average of each feature for each subject and activity
 combined.set.for.subject.and.activity <- group_by(combined.set, Subject, Activity) %>% summarise_each(funs(mean))
+
 
 # output tidy data set
 write.table(combined.set.for.subject.and.activity,
